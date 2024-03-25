2931227b5f7abef3eb1bba38038597a729d4fa28
VithorLeonardoMS <162487463+VithorLeonardoMS@users.noreply.github.com>
2024-03-25 17:06:03 -0300

    Add files via upload

--- 25.03.24/main.js
+++ 25.03.24/main.js
@@ -1,15 +1,41 @@
-var numero1 = 5;
-var numero2 = 8;
-var resultado;
-resultado = numero1 == numero2;
-print(numero1,' = ', numero2, '?',resultado);
-resultado = numero1 != numero2;
-print(numero1, '!=', numero2, '?', resultado);
-resultado = numero1 > numero2
-print(numero1, '>', numero2, '?', resultado);
-resultado = numero1 < numero2
-print(numero1, '<', numero2, '?', resultado);
-resultado = numero1 >= numero2;
-print(numero1, '>=', numero2, '?', resultado);
-resultado = numero1 <= numero2;
-print(numero1, '<=', numero2, '?', resultado);
+
+var a = 9
+var b = 10
+var c = 9
+let d = 5
+if((a>10 && a<20) || (b === 5)){
+print('true')
+} else {
+    print('false')
+}
+
+//2
+if(((a>10) || (b == 5))){
+    print('true')
+} else{
+    print('false')
+    }
+    
+
+//3
+if(a>10 && a<20){
+    print('true')
+} else{
+    print('false')
+    }
+    
+
+//4
+if((a>10)||(b===5) && (c===10) ){
+    print('true')
+} else{
+    print('false')
+    }
+    
+//5
+if (((a>10)||(b ===5))&& (c !=10) && (d!=20)){
+    print('true')
+} else {
+    print('false')
+}
+
